692aaeb029ddf35874255719208425c470838951
Adolfo A. Maza Peña <129304949+aamzp@users.noreply.github.com>
2025-11-15 20:30:25 -0300

    update

--- miNoise-frontend/vite.config.ts
+++ miNoise-frontend/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/miNoise/visualizer/",   // 👈 GitHub Pages LO NECESITA
   plugins: [react()],
-})
+  build: {
+    outDir: "../docs/visualizer",
+    emptyOutDir: true,
+  }
+});
