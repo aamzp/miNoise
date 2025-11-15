273e23033edcd49a1550e99919ce3ccb983c9820
Adolfo A. Maza Peña <129304949+aamzp@users.noreply.github.com>
2025-11-15 20:48:46 -0300

    Fix: correct data URL for Github Pages

--- miNoise-frontend/src/components/Graph3D/Graph3D.tsx
+++ miNoise-frontend/src/components/Graph3D/Graph3D.tsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 import { initGraph3D } from "./initGraph3D";
 
-const DATA_URL = "/miNoise/visualizer/data/tag_album_embedded_graph_FINAL.json";
+const DATA_URL = new URL("./data/tag_album_embedded_graph_FINAL.json", import.meta.url).href;
 
 
 
